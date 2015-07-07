03f2259ffb1cb292eacec211b969bb6031c55696
Abhay Dekate <AbhayKD@users.noreply.github.com>
2015-07-08 00:38:49 +0530

    Calculator test added with the junit

---
+++ Calc.java
@@ -0,0 +1,11 @@
+package com.Abhay.Practice;
+
+public class Calc {
+
+	public static int square(int number) {
+		int square;
+		square = number *number;
+		return square;
+	}
+
+}
---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package com.Abhay.Practice;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Calculator {
+/*SUT- System Under Test*/
+	@Test
+	public void test(){
+		Calc obj = new Calc();
+		assertEquals(25,Calc.square(5));
+		assertEquals(36,Calc.square(6));
+
+	}
+
+}
